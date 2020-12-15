29213c93b71291bc9496186e7bdc69f0395cc4cc
Ben Hayes <b.t.hayes@comcast.net>
2020-12-15 19:05:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(number)
   if number <= 1 
     return false 
   else 
-    if (4..(number-1)).any? {|i| number % i == 0}
+    if (2..(number-1)).any? {|i| number % i == 0}
       return false
     else
       return true 
