a4c6c567e43c32b24255c87b240ca23d6e5fedfb
Ben Hayes <b.t.hayes@comcast.net>
2020-12-15 18:58:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,4 @@
 def prime?(number)
-  array = (2..9).to_a
-  new_array = array.map{|element| number % element}
-  if !new_array.include?(0) && number > 1 
-    true 
-  else
-    false
-  end
+  if number <= 1 
+    
 end
